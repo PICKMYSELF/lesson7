4493a0dbcf56957ad722c9616961223080b30b6f
VoidWatcher <VoidWatcher@users.noreply.github.com>
2017-06-08 21:49:42 +0800

    5

--- main/main.js
+++ main/main.js
@@ -3,5 +3,5 @@ module.exports = function main(email, suffixes) {
   var email_words = email.split("@");
   console.log(email_words.slice(-1) + ' ' + suffixes);
   console.log(email_words.slice(-1) == suffixes);
-  return email_words.slice(-1) == suffixes;
+  return String(email_words.slice(-1)) == String(suffixes);
 };
